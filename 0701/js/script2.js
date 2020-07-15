199fbb22ba7fe7daa2d0b393dac1ebab93559af7
ckg20068 <ckg20068@st.ckg.ac.jp>
2020-07-15 17:02:46 +0900

    2020-0715-1659

---
+++ 0701/js/script2.js
@@ -0,0 +1,23 @@
+document.write( "<hr>")
+
+var i,j
+
+i = 9 ;
+j = 9 ;
+
+for ( j = 1; j<=9 ; j++){
+    for ( i = 1; i<=9 ; i++){
+    document.write( "<div>")
+    document.write( ( i * j))
+    document.write( "</div>")
+    }
+}
+    
+
+
+
+
+
+
+
+document.write( "<hr>")
